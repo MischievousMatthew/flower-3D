e3093661a46d71a3a7bb668bfe19e23194448605
MischievousMatthew <javierjohnmatthew6@gmail.com>
2026-03-26 18:06:59 +0800

    Final connect frontend to backend API

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const baseURL =
-  import.meta.env.production.VITE_API_BASE_URL || "http://localhost:8000/api";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
 
 const api = axios.create({
   baseURL,
