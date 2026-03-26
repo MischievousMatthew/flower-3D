08b42082be160ef9586b4471b9834aeeb00f38f6
MischievousMatthew <javierjohnmatthew6@gmail.com>
2026-03-26 22:51:10 +0800

    FIX env

--- backend/app/Mail/OtpMail.php
+++ backend/app/Mail/OtpMail.php
@@ -3,23 +3,36 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use SendinBlue\Client\Api\TransactionalEmailsApi;
+use SendinBlue\Client\Configuration;
+use SendinBlue\Client\Model\SendSmtpEmail;
 
-class OtpMail extends Mailable implements ShouldQueue
+class OtpMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public function __construct(
-        public readonly string $otp,
-        public readonly int    $expiryMinutes = 5,
-    ) {}
+    public $otp;
 
-    public function build(): self
+    public function __construct($otp)
     {
-        return $this
-            ->subject('Your Bloomcraft verification code')
-            ->view('emails.otp');
+        $this->otp = $otp;
+    }
+
+    public function build()
+    {
+        $config = Configuration::getDefaultConfiguration()->setApiKey('api-key', env('BREVO_API_KEY'));
+        $apiInstance = new TransactionalEmailsApi(null, $config);
+
+        $email = new SendSmtpEmail([
+            'to' => [['email' => $this->to[0]['address'], 'name' => $this->to[0]['name'] ?? 'User']],
+            'templateId' => null,
+            'params' => ['otp' => $this->otp],
+            'sender' => ['email' => env('MAIL_FROM_ADDRESS'), 'name' => env('MAIL_FROM_NAME')],
+            'subject' => 'Your OTP Code'
+        ]);
+
+        $apiInstance->sendTransacEmail($email);
     }
 }
